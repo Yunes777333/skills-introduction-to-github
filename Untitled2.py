9c756d98ac58f63f6d26bf17dba7ca600a368ac6
Yunes777333 <unesbahteev@gmail.com>
2025-02-15 11:18:02 +0300

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,31 @@
+class BinaryTree:
+    def deleteMode(self, root, data):
+        if root is None:
+            return root
+
+        if data < root.data:
+            root.left = self.deleteMode(root.left, data)    
+        elif data > root.data:
+            root.right = self.deleteMode(root.right, data)  
+        else:
+            if root.left is None and root.right is None
+                return None
+            elif root.left is None
+                return root.right
+            elif root.right is None
+                return root.left
+            else:
+                root.data = self.getMin(root.right).data
+                root.right = self.deleteMode(root.right, root.data)
+
+        return root
+    def getMin(self, root):
+        if root is None:
+            return root
+
+        while root.left is not None
+            root = root.left
+    
+        return root
+
+
